025fc04a23c692617c469f7131c97973c439ae19
LuisAntesUS13 <leantesv@desarrollo-ultrasist.com.mx>
2024-09-24 10:44:05 -0600

    Elaboración de menú y rutas predefinidas

--- front-gention-bitacoras/src/App.js
+++ front-gention-bitacoras/src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import { Enrutador } from "./app/Routes/Route.tsx";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Hola mundo</p>
+        <BrowserRouter>
+          <Enrutador />
+        </BrowserRouter>
       </header>
     </div>
   );
---
+++ front-gention-bitacoras/src/app/shared/SubMenu/Submenu.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Button, Menu, MenuItem } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+
+interface IProps {
+  titulo: string;
+  opciones?: { subtitulo: string; ruta: string }[];
+}
+
+export const Submenu = ({ titulo, opciones }: IProps) => {
+  const validacion = opciones && opciones.length > 0;
+
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    if (!validacion) {
+      navigate("/Requerimientos");
+    }
+
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <Button
+        id="basic-button"
+        aria-controls={open ? "basic-menu" : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? "true" : undefined}
+        onClick={handleClick}
+        endIcon={validacion && <KeyboardArrowDownIcon />}
+      >
+        {titulo}
+      </Button>
+
+      {validacion && (
+        <Menu
+          id="basic-menu"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          MenuListProps={{
+            "aria-labelledby": "basic-button",
+          }}
+        >
+          {opciones.map((opcion) => (
+            <MenuItem onClick={() => navigate(`/${opcion.ruta}`)}>
+              {opcion.subtitulo}
+            </MenuItem>
+          ))}
+        </Menu>
+      )}
+    </div>
+  );
+};
