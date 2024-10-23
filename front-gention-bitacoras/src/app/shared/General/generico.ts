422640b72961e4042d7e1a94703c60609d8cc45b
Miguel Angel <miguelaes_@hotmail.com>
2024-10-23 01:21:21 -0600

    s

--- front-gention-bitacoras/src/app/shared/General/generico.ts
+++ front-gention-bitacoras/src/app/shared/General/generico.ts
@@ -1,14 +1,14 @@
 export function convertirNumeroSeparadoComas(numero : string) : string {
-    let monto_variable = numero;
+    let monto = numero;
     try {
         const [integerPart, decimalPart] = numero.toString().split(".");
         const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
         // Unir la parte entera formateada con la parte decimal (si existe)
-         monto_variable =  decimalPart !== undefined ? `${formattedInteger}.${decimalPart}` : formattedInteger;
+        monto =  decimalPart !== undefined ? `${formattedInteger}.${decimalPart}` : formattedInteger;
     } catch (ex) {
-        monto_variable = numero;
+        monto = numero;
     }
-    return monto_variable
+    return monto
 }
   
 
